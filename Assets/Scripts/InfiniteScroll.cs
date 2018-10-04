10915851935a4fe332887c0bf55b5498a6e351be
eclancy <eclancy68@gmail.com>
2018-10-03 22:58:50 -0400

    Fixed spacing issue between background tiles

--- Assets/Scripts/InfiniteScroll.cs
+++ Assets/Scripts/InfiniteScroll.cs
@@ -18,7 +18,7 @@ public class InfiniteScroll : MonoBehaviour
         transform.position = currentPosition - Vector3.right * offset;
         if(transform.position.x < 0)
         {
-            Vector3 temp = new Vector3(34, 0, 0);
+            Vector3 temp = new Vector3(30, 0, 0);
             transform.position += temp;
         }
         currentPosition = transform.position;
