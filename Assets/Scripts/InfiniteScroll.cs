ea2192478b086f1f45e480879e8218e067b53fb7
Bubier <skyler.bubier@tylertech.com>
2018-10-05 00:24:40 -0400

    Various changes for Player Controls
    Lerp interpolation for centering player movement, a toggle for locking player movement (for level end, etc.). Also various changes on background tiling, some cleanup of Assets, a test Player starship asset as well as Object heirarchy maintenance.

--- Assets/Scripts/InfiniteScroll.cs
+++ Assets/Scripts/InfiniteScroll.cs
@@ -3,25 +3,21 @@ using System.Collections;
 
 public class InfiniteScroll : MonoBehaviour
 {
-    private float scrollSpeed = 1;
-    private Vector3 currentPosition;
+    private const float SCROLL_DAMP = 0.1f;
+    private Renderer _renderer;
 
-    void Start()
+    public float scrollSpeed = 1f;
+
+    private void Start()
     {
-        currentPosition = transform.position;
+        // Grab Renderer of Scrollin' Object
+        _renderer = GetComponent<Renderer>();
     }
 
-    void Update()
+    private void Update()
     {
-        float offset = Time.deltaTime * scrollSpeed;
-        //Debug.Log(offset);
-        transform.position = currentPosition - Vector3.right * offset;
-        if(transform.position.x < 0)
-        {
-            Vector3 temp = new Vector3(30, 0, 0);
-            transform.position += temp;
-        }
-        currentPosition = transform.position;
-
+        // Move the offset of the tiling, then repeat on 1
+        float offsetX = Mathf.Repeat(Time.time * scrollSpeed * SCROLL_DAMP, 1f);
+        _renderer.material.SetTextureOffset("_MainTex", new Vector2(offsetX, 0));
     }
 }
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,47 +6,109 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
 
-    public float minY = -3.75f;
-    public float maxY = 3.75f;
+    // Movement Constants
+    private const float CENTERING_THRESHOLD = 0.01f;
+    private const float CENTERING_SPEED = 0.1f;
 
+    // Player Movement Bounds
+    public float moveMinY = -3.75f;
+    public float moveMaxY = 3.75f;
     public float keyMoveSpeed = 0.3f;
+    public float touchChaseSpeed = 0.2f;
 
-    private void Start ()
+    private bool _isCentering = false;
+
+    public bool IsCentering
     {
-        
+        get { return _isCentering; }
+    }
+
+    private bool _isMovementLocked = false;
+
+    public bool IsMovementLocked
+    {
+        get { return _isMovementLocked; }
+        set
+        {
+            _isMovementLocked = value;
+            if (!value)
+                _isCentering = false;
+        }
+    }
+
+    public void CenterPlayer()
+    {
+        _isCentering = true;
     }
 
     private void Update ()
     {
-
-        // Keyboard Input for PC Testing
-        if (Input.GetKey(KeyCode.W))
+        if (_isCentering)
         {
+            // Center Player
             transform.position = new Vector3(
                 transform.position.x,
-                transform.position.y + keyMoveSpeed,
+                Mathf.Lerp(transform.position.y, 0, CENTERING_SPEED),
                 transform.position.z);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.position = new Vector3(
-                transform.position.x,
-                transform.position.y - keyMoveSpeed,
-                transform.position.z);
-        }
 
-        // Handle Touch Input
-        for (int i = 0; i < Input.touches.Length; i++)
+            // Finish Centering once close enough
+            if (Mathf.Abs(transform.position.y) < CENTERING_THRESHOLD)
+            {
+                _isCentering = false;
+            }
+        }
+        else if (!_isMovementLocked)
         {
-            var touch = Input.GetTouch(i);
+            // Keyboard Input for PC Testing
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position = new Vector3(
+                    transform.position.x,
+                    transform.position.y + keyMoveSpeed,
+                    transform.position.z);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.position = new Vector3(
+                    transform.position.x,
+                    transform.position.y - keyMoveSpeed,
+                    transform.position.z);
+            }
 
-            Debug.Log(string.Format("I am being touched by {0} in the {1}.", i, touch.position));
+            // TODO: Remove
+            // TEST Center Player
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                CenterPlayer();
+            }
+
+            // Handle Touch Input
+            for (int i = 0; i < Input.touches.Length; i++)
+            {
+                var touch = Input.GetTouch(i);
+
+                Debug.Log(string.Format("I am being touched by {0} in the {1}.", i, touch.position));
+
+                transform.position = new Vector3(
+                    transform.position.x,
+                    Mathf.Lerp(transform.position.y, touch.position.y, touchChaseSpeed),
+                    transform.position.z);
+            }
+
+            // 
+            if (Input.GetKey(KeyCode.R))
+            {
+                transform.position = new Vector3(
+                    transform.position.x,
+                    Mathf.Lerp(transform.position.y, 0, touchChaseSpeed),
+                    transform.position.z);
+            }
         }
 
         // Clamp Player Position
         transform.position = new Vector3(
             transform.position.x,
-            Mathf.Clamp(transform.position.y, minY, maxY),
+            Mathf.Clamp(transform.position.y, moveMinY, moveMaxY),
             transform.position.z);
     }
 
