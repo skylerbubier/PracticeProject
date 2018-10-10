85619d9d41fc25dcc51f397102c264aeaf9ee5b2
skylerbubier <43798021+skylerbubier@users.noreply.github.com>
2018-10-10 16:54:01 -0400

    Player movement improvements
    Resolves #17

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,25 +6,46 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
 
+    #region const
+
     // Movement Constants
     private const float CENTERING_THRESHOLD = 0.01f;
     private const float CENTERING_SPEED = 0.1f;
+    private const float ROTATE_RESET_SPEED = 0.1f;
+
+    #endregion
+
+    #region private
+
+    // Private
+    private PlayerShip _ship;
+    private bool _isCentering = false;
+    private bool _isMovementLocked = false;
+    private bool _isFiring = false;
+    private float _fireElapsed = 0f;
+
+    #endregion
 
     // Player Movement Bounds
     public float moveMinY = -3.75f;
     public float moveMaxY = 3.75f;
     public float keyMoveSpeed = 0.3f;
     public float touchChaseSpeed = 0.2f;
+    public float fireRate = 1f;
 
-    private bool _isCentering = false;
-
+    /// <summary>
+    /// Returns if the player is in a "reset mode" and the ship is moving to center
+    /// to the screen. Movement will be locked in this time.
+    /// </summary>
     public bool IsCentering
     {
         get { return _isCentering; }
     }
 
-    private bool _isMovementLocked = false;
-
+    /// <summary>
+    /// Returns if the player cannot control the ship. This is used when the ship
+    /// is Centering or at the beginning / end of the scene.
+    /// </summary>
     public bool IsMovementLocked
     {
         get { return _isMovementLocked; }
@@ -36,23 +57,44 @@ public class PlayerControls : MonoBehaviour
         }
     }
 
+    public bool IsFiring
+    {
+        get { return _isFiring; }
+        set { _isFiring = true; }
+    }
+    
+    /// <summary>
+    /// Locks Movement until the player's ship is Centered. Use IsMovementLocked = true to keep the lock.
+    /// </summary>
     public void CenterPlayer()
     {
         _isCentering = true;
     }
 
-    private void Update ()
+    private void Start()
     {
+        _ship = gameObject.GetComponentInChildren<PlayerShip>();
+    }
+
+    private void Update()
+    {
+        Update_Movement();
+        Update_Firing();
+    }
+
+    private void Update_Movement()
+    {
+        var posYOrig = transform.position.y;
+        var posYNew = posYOrig;
+
+
         if (_isCentering)
         {
             // Center Player
-            transform.position = new Vector3(
-                transform.position.x,
-                Mathf.Lerp(transform.position.y, 0, CENTERING_SPEED),
-                transform.position.z);
+            posYNew = Mathf.Lerp(transform.position.y, 0, CENTERING_SPEED);
 
             // Finish Centering once close enough
-            if (Mathf.Abs(transform.position.y) < CENTERING_THRESHOLD)
+            if (Mathf.Abs(posYNew) < CENTERING_THRESHOLD)
             {
                 _isCentering = false;
             }
@@ -62,24 +104,11 @@ public class PlayerControls : MonoBehaviour
             // Keyboard Input for PC Testing
             if (Input.GetKey(KeyCode.W))
             {
-                transform.position = new Vector3(
-                    transform.position.x,
-                    transform.position.y + keyMoveSpeed,
-                    transform.position.z);
+                posYNew += keyMoveSpeed;
             }
             else if (Input.GetKey(KeyCode.S))
             {
-                transform.position = new Vector3(
-                    transform.position.x,
-                    transform.position.y - keyMoveSpeed,
-                    transform.position.z);
-            }
-
-            // TODO: Remove
-            // TEST Center Player
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                CenterPlayer();
+                posYNew -= keyMoveSpeed;
             }
 
             // Handle Touch Input
@@ -89,20 +118,27 @@ public class PlayerControls : MonoBehaviour
 
                 Debug.Log(string.Format("I am being touched by {0} in the {1}.", i, touch.position));
 
-                transform.position = new Vector3(
-                    transform.position.x,
-                    Mathf.Lerp(transform.position.y, touch.position.y, touchChaseSpeed),
-                    transform.position.z);
+                posYNew = Mathf.Lerp(transform.position.y, touch.position.y, touchChaseSpeed);
             }
 
-            // 
-            if (Input.GetKey(KeyCode.R))
-            {
-                transform.position = new Vector3(
-                    transform.position.x,
-                    Mathf.Lerp(transform.position.y, 0, touchChaseSpeed),
-                    transform.position.z);
-            }
+        }
+
+        // Set New Player Position
+        transform.position = new Vector3(
+            transform.position.x,
+            posYNew,
+            transform.position.z);
+
+        // Tilt the ship to simulate fluid movement
+        if (_ship != null)
+        {
+            var posYdelta = Mathf.Clamp(posYNew - posYOrig, -90f, 90f);
+            _ship.transform.rotation = new Quaternion(
+                Mathf.Lerp(_ship.transform.rotation.x, posYdelta, ROTATE_RESET_SPEED),
+                _ship.transform.rotation.y,
+                _ship.transform.rotation.z,
+                _ship.transform.rotation.w);
+
         }
 
         // Clamp Player Position
@@ -110,6 +146,27 @@ public class PlayerControls : MonoBehaviour
             transform.position.x,
             Mathf.Clamp(transform.position.y, moveMinY, moveMaxY),
             transform.position.z);
+    }
+
+    private void Update_Firing()
+    {
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            _isFiring = !_isFiring;
+        }
+
+        if (_isFiring)
+        {
+            _fireElapsed += Time.deltaTime;
+            if (_fireElapsed >= fireRate)
+            {
+                _fireElapsed -= fireRate;
+
+                //TODO: Spawn lasers!
+
+            }
+        }
     }
 
 }
---
+++ Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class PlayerShip : MonoBehaviour
+{
+
+    
+
+}
